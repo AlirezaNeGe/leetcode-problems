d88c8c0fe335a55cffde139b85599f66482a495a
Alireza <alirezanege.1404@gmail.com>
2024-03-28 19:15:37 +0330

    Add solution for Add Two Numbers

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ These are my solutions to leetcode problems.
 Solved problems:
 
 - [Two Sum](/golang/two-sum/main.go) _(solved using golang)_
+- [Add Two Numbers](/c/add-two-numbers/main.c) _(solved using c)_
---
+++ c/add-two-numbers/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
+    struct ListNode *result = malloc(sizeof(struct ListNode));
+    struct ListNode *tail = result;
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int sum = 0;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        sum += carry;
+        carry = sum / 10;
+        sum %= 10;
+
+        struct ListNode *newNode = malloc(sizeof(struct ListNode));
+        newNode->val = sum;
+        newNode->next = NULL;
+
+        tail->next = newNode;
+        tail = tail->next;
+    }
+
+    return result->next;
+}
+
+int main() {
+    // linked list
+    struct ListNode *head1 = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head1->val = 2;
+    head1->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head1->next->val = 4;
+    head1->next->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head1->next->next->val = 3;
+    head1->next->next->next = NULL;
+
+    // linked list
+    struct ListNode *head2 = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head2->val = 5;
+    head2->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head2->next->val = 6;
+    head2->next->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+    head2->next->next->val = 4;
+    head2->next->next->next = NULL;
+
+    struct ListNode *result = addTwoNumbers(head1, head2);
+    printf("result = %d\n", result->val);
+
+    return 0;
+}
